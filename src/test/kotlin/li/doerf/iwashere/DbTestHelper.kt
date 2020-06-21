f2e272b66bde77db23741e2ac3e8aeae4441ef7d
Marc Doerflinger <github@doerf.li>
2020-06-21 22:36:24 +0200

    fix test

--- src/test/kotlin/li/doerf/iwashere/DbTestHelper.kt
+++ src/test/kotlin/li/doerf/iwashere/DbTestHelper.kt
@@ -3,6 +3,8 @@ package li.doerf.iwashere
 import li.doerf.iwashere.entities.User
 import li.doerf.iwashere.repositories.LocationRepository
 import li.doerf.iwashere.repositories.UserRepository
+import li.doerf.iwashere.repositories.VisitRepository
+import li.doerf.iwashere.repositories.VisitorRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -13,8 +15,14 @@ class DbTestHelper {
     private lateinit var userRepository: UserRepository
     @Autowired
     private lateinit var locationRepository: LocationRepository
+    @Autowired
+    private lateinit var visitorRepository: VisitorRepository
+    @Autowired
+    private lateinit var visitRepository: VisitRepository
 
     fun cleanDb() {
+        visitRepository.deleteAll()
+        visitorRepository.deleteAll()
         locationRepository.deleteAll()
         userRepository.deleteAll()
     }
