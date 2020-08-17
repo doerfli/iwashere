dab202b0d9dfc59e2bdc088a12799d113aaff771
Marc Doerflinger <marc@doerf.li>
2020-08-17 22:27:31 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${Versions.coroutinesVersion}")
 	implementation("org.thymeleaf:thymeleaf-spring5")
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,18 +1,18 @@
 object Versions {
 
     // plugins
-    const val kotlin = "1.3.72"
-    const val spring = "2.3.1.RELEASE"
-    const val springDependencyManagement = "1.0.9.RELEASE"
-    const val benManesVersions = "0.28.0"
-    const val nebulaRelease = "15.0.3"
+    const val kotlin = "1.4.0"
+    const val spring = "2.3.3.RELEASE"
+    const val springDependencyManagement = "1.0.10.RELEASE"
+    const val benManesVersions = "0.29.0"
+    const val nebulaRelease = "15.1.0"
 
     // dependencies
     const val fuel = "2.2.3"
     const val javaDotenv = "5.2.1"
-    const val mockk = "2.0.2"
-    const val coroutinesVersion = "1.3.7"
-    const val springDocOpenApi = "1.4.1"
+    const val mockk = "2.0.3"
+    const val coroutinesVersion = "1.3.9"
+    const val springDocOpenApi = "1.4.4"
     const val javaFaker = "1.0.2"
     const val ktor = "1.3.2"
     const val openCSV = "5.2"
