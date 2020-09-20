6878f210504c7cd59dc3e6366a510bdcdac5663a
Marc Doerflinger <marc@doerf.li>
2020-09-20 21:32:51 +0200

    update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,17 +2,17 @@ object Versions {
 
     // plugins
     const val kotlin = "1.4.10"
-    const val spring = "2.3.3.RELEASE"
+    const val spring = "2.3.4.RELEASE"
     const val springDependencyManagement = "1.0.10.RELEASE"
-    const val benManesVersions = "0.31.0"
-    const val nebulaRelease = "15.1.0"
+    const val benManesVersions = "0.33.0"
+    const val nebulaRelease = "15.2.0"
 
     // dependencies
     const val fuel = "2.2.3"
     const val javaDotenv = "5.2.2"
     const val mockk = "2.0.3"
     const val coroutinesVersion = "1.3.9"
-    const val springDocOpenApi = "1.4.4"
+    const val springDocOpenApi = "1.4.6"
     const val javaFaker = "1.0.2"
     const val ktor = "1.4.0"
     const val openCSV = "5.2"
